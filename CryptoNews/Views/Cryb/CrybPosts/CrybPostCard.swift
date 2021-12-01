7158ba6311ecb55a13e0b51b3f7d5c49f4c21081
Krishna Venkatramani <krishsgithub1@gmail.com>
2021-12-01 11:25:30 +0400

    Creating Crybost

---
+++ CryptoNews/Views/Cryb/CrybPosts/CrybPostCard.swift
@@ -0,0 +1,20 @@
+//
+//  CrybPostCard.swift
+//  CryptoNews
+//
+//  Created by Krishna Venkatramani on 01/12/2021.
+//
+
+import SwiftUI
+
+struct CrybPostCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CrybPostCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CrybPostCard()
+    }
+}
