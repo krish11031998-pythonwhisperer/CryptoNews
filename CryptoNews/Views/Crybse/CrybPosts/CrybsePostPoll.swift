c9ac448f00125eca3b593d6c2e266c0ef9d5466a
Krishna Venkatramani <krish.fabre@gmail.com>
2022-02-04 18:38:36 +0400

    Refactoring and pull merge from V2-CrybseServer-Simplified

---
+++ CryptoNews/Views/Crybse/CrybPosts/CrybsePostPoll.swift
@@ -0,0 +1,20 @@
+//
+//  CrybsePostPoll.swift
+//  CryptoNews
+//
+//  Created by Krishna Venkatramani on 04/02/2022.
+//
+
+import SwiftUI
+
+struct CrybsePostPoll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CrybsePostPoll_Previews: PreviewProvider {
+    static var previews: some View {
+        CrybsePostPoll()
+    }
+}
